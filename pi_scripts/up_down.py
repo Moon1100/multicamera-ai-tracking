07b38f1aded49a349c487a21a36a717a7639fc4b
Muhammad Aliff Mohamed Anuar <75883097+17203058@users.noreply.github.com>
2023-05-11 12:45:45 +0800

    up_down.py

--- pi_scripts/up_down.py
+++ pi_scripts/up_down.py
@@ -1,107 +1,96 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/python
+
 import time
+import math
+import smbus
 
-class AlphaBot2(object):
+# ============================================================================
+# Raspi PCA9685 16-Channel PWM Servo Driver
+# ============================================================================
+
+class PCA9685:
+
+  # Registers/etc.
+  __SUBADR1            = 0x02
+  __SUBADR2            = 0x03
+  __SUBADR3            = 0x04
+  __MODE1              = 0x00
+  __PRESCALE           = 0xFE
+  __LED0_ON_L          = 0x06
+  __LED0_ON_H          = 0x07
+  __LED0_OFF_L         = 0x08
+  __LED0_OFF_H         = 0x09
+  __ALLLED_ON_L        = 0xFA
+  __ALLLED_ON_H        = 0xFB
+  __ALLLED_OFF_L       = 0xFC
+  __ALLLED_OFF_H       = 0xFD
+
+  def __init__(self, address=0x40, debug=False):
+    self.bus = smbus.SMBus(1)
+    self.address = address
+    self.debug = debug
+    if (self.debug):
+      print("Reseting PCA9685")
+    self.write(self.__MODE1, 0x00)
 	
-	def __init__(self,ain1=12,ain2=13,ena=6,bin1=20,bin2=21,enb=26):
-		self.AIN1 = ain1
-		self.AIN2 = ain2
-		self.BIN1 = bin1
-		self.BIN2 = bin2
-		self.ENA = ena
-		self.ENB = enb
-		self.PA  = 50
-		self.PB  = 50
+  def write(self, reg, value):
+    "Writes an 8-bit value to the specified register/address"
+    self.bus.write_byte_data(self.address, reg, value)
+    if (self.debug):
+      print("I2C: Write 0x%02X to register 0x%02X" % (value, reg))
+	  
+  def read(self, reg):
+    "Read an unsigned byte from the I2C device"
+    result = self.bus.read_byte_data(self.address, reg)
+    if (self.debug):
+      print("I2C: Device 0x%02X returned 0x%02X from reg 0x%02X" % (self.address, result & 0xFF, reg))
+    return result
+	
+  def setPWMFreq(self, freq):
+    "Sets the PWM frequency"
+    prescaleval = 25000000.0    # 25MHz
+    prescaleval /= 4096.0       # 12-bit
+    prescaleval /= float(freq)
+    prescaleval -= 1.0
+    if (self.debug):
+      print("Setting PWM frequency to %d Hz" % freq)
+      print("Estimated pre-scale: %d" % prescaleval)
+    prescale = math.floor(prescaleval + 0.5)
+    if (self.debug):
+      print("Final pre-scale: %d" % prescale)
 
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setwarnings(False)
-		GPIO.setup(self.AIN1,GPIO.OUT)
-		GPIO.setup(self.AIN2,GPIO.OUT)
-		GPIO.setup(self.BIN1,GPIO.OUT)
-		GPIO.setup(self.BIN2,GPIO.OUT)
-		GPIO.setup(self.ENA,GPIO.OUT)
-		GPIO.setup(self.ENB,GPIO.OUT)
-		self.PWMA = GPIO.PWM(self.ENA,500)
-		self.PWMB = GPIO.PWM(self.ENB,500)
-		self.PWMA.start(self.PA)
-		self.PWMB.start(self.PB)
-		self.stop()
+    oldmode = self.read(self.__MODE1);
+    newmode = (oldmode & 0x7F) | 0x10        # sleep
+    self.write(self.__MODE1, newmode)        # go to sleep
+    self.write(self.__PRESCALE, int(math.floor(prescale)))
+    self.write(self.__MODE1, oldmode)
+    time.sleep(0.005)
+    self.write(self.__MODE1, oldmode | 0x80)
 
-	def forward(self):
-		self.PWMA.ChangeDutyCycle(self.PA)
-		self.PWMB.ChangeDutyCycle(self.PB)
-		GPIO.output(self.AIN1,GPIO.LOW)
-		GPIO.output(self.AIN2,GPIO.HIGH)
-		GPIO.output(self.BIN1,GPIO.LOW)
-		GPIO.output(self.BIN2,GPIO.HIGH)
-
-
-	def stop(self):
-		self.PWMA.ChangeDutyCycle(0)
-		self.PWMB.ChangeDutyCycle(0)
-		GPIO.output(self.AIN1,GPIO.LOW)
-		GPIO.output(self.AIN2,GPIO.LOW)
-		GPIO.output(self.BIN1,GPIO.LOW)
-		GPIO.output(self.BIN2,GPIO.LOW)
-
-	def backward(self):
-		self.PWMA.ChangeDutyCycle(self.PA)
-		self.PWMB.ChangeDutyCycle(self.PB)
-		GPIO.output(self.AIN1,GPIO.HIGH)
-		GPIO.output(self.AIN2,GPIO.LOW)
-		GPIO.output(self.BIN1,GPIO.HIGH)
-		GPIO.output(self.BIN2,GPIO.LOW)
-
-		
-	def left(self):
-		self.PWMA.ChangeDutyCycle(30)
-		self.PWMB.ChangeDutyCycle(30)
-		GPIO.output(self.AIN1,GPIO.HIGH)
-		GPIO.output(self.AIN2,GPIO.LOW)
-		GPIO.output(self.BIN1,GPIO.LOW)
-		GPIO.output(self.BIN2,GPIO.HIGH)
-
-
-	def right(self):
-		self.PWMA.ChangeDutyCycle(30)
-		self.PWMB.ChangeDutyCycle(30)
-		GPIO.output(self.AIN1,GPIO.LOW)
-		GPIO.output(self.AIN2,GPIO.HIGH)
-		GPIO.output(self.BIN1,GPIO.HIGH)
-		GPIO.output(self.BIN2,GPIO.LOW)
-		
-	def setPWMA(self,value):
-		self.PA = value
-		self.PWMA.ChangeDutyCycle(self.PA)
-
-	def setPWMB(self,value):
-		self.PB = value
-		self.PWMB.ChangeDutyCycle(self.PB)	
-		
-	def setMotor(self, left, right):
-		if((right >= 0) and (right <= 100)):
-			GPIO.output(self.AIN1,GPIO.HIGH)
-			GPIO.output(self.AIN2,GPIO.LOW)
-			self.PWMA.ChangeDutyCycle(right)
-		elif((right < 0) and (right >= -100)):
-			GPIO.output(self.AIN1,GPIO.LOW)
-			GPIO.output(self.AIN2,GPIO.HIGH)
-			self.PWMA.ChangeDutyCycle(0 - right)
-		if((left >= 0) and (left <= 100)):
-			GPIO.output(self.BIN1,GPIO.HIGH)
-			GPIO.output(self.BIN2,GPIO.LOW)
-			self.PWMB.ChangeDutyCycle(left)
-		elif((left < 0) and (left >= -100)):
-			GPIO.output(self.BIN1,GPIO.LOW)
-			GPIO.output(self.BIN2,GPIO.HIGH)
-			self.PWMB.ChangeDutyCycle(0 - left)
+  def setPWM(self, channel, on, off):
+    "Sets a single PWM channel"
+    self.write(self.__LED0_ON_L+4*channel, on & 0xFF)
+    self.write(self.__LED0_ON_H+4*channel, on >> 8)
+    self.write(self.__LED0_OFF_L+4*channel, off & 0xFF)
+    self.write(self.__LED0_OFF_H+4*channel, off >> 8)
+    if (self.debug):
+      print("channel: %d  LED_ON: %d LED_OFF: %d" % (channel,on,off))
+	  
+  def setServoPulse(self, channel, pulse):
+    "Sets the Servo Pulse,The PWM frequency must be 50HZ"
+    pulse = int(pulse*4096/20000)        #PWM frequency is 50HZ,the period is 20000us
+    self.setPWM(channel, 0, pulse)
 
 if __name__=='__main__':
-
-	Ab = AlphaBot2()
-	Ab.forward()
-	try:
-		while True:
-			time.sleep(1)
-	except KeyboardInterrupt:
-		GPIO.cleanup()
+ 
+  pwm = PCA9685(0x40, debug=True)
+  pwm.setPWMFreq(50)
+  while True:
+   # setServoPulse(2,2500)
+    for i in range(500,2500,10):  
+      pwm.setServoPulse(0,i)   
+      time.sleep(0.02)     
+    
+    for i in range(2500,500,-10):
+      pwm.setServoPulse(0,i) 
+      time.sleep(0.02)  
