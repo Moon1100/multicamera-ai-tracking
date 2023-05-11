9d5dc955d27e51ca2cedb4abc689ada53269c0cc
Muhammad Aliff Mohamed Anuar <75883097+17203058@users.noreply.github.com>
2023-05-11 13:34:38 +0800

    new manuever

--- pi_scripts/main.py
+++ pi_scripts/main.py
@@ -16,7 +16,7 @@ HStep = 0      # Sets the initial step length
 VPulse = 1500  # Sets the initial Pulse
 VStep = 0    # Sets the initial step length (constant movement up)
 pwm.setServoPulse(1, VPulse)
-pwm.setServoPulse(1, HPulse)
+pwm.setServoPulse(0, HPulse)
 
 
 
