b912b899aa17771ea70e3d2319aa3acc070ea347
Muhammad Aliff Mohamed Anuar <75883097+17203058@users.noreply.github.com>
2023-05-16 11:56:39 +0800

    pubsub update

--- pi_scripts/subscriber.py
+++ pi_scripts/subscriber.py
@@ -1,11 +1,32 @@
+# import redis
+
+# r = redis.Redis(host='localhost', port=6379, db=0)
+# p = r.pubsub()
+# # p.subscribe('my_channel')#for testing socket
+# p.subscribe('1')
+
+
+# for message in p.listen():
+#     if message['type'] == 'message':
+#         print(message['data'].decode())
+
+
 import redis
 
-r = redis.Redis(host='localhost', port=6379, db=0)
-p = r.pubsub()
-# p.subscribe('my_channel')#for testing socket
-p.subscribe('1')
+def listen_messages(channel):
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    pubsub = r.pubsub()
 
+    # Subscribe to the specified channel
+    pubsub.subscribe(channel)
 
-for message in p.listen():
-    if message['type'] == 'message':
-        print(message['data'].decode())
+    print(f"Listening to channel: {channel}")
+
+    # Start listening to messages
+    for message in pubsub.listen():
+        if message['type'] == 'message':
+            print(f"Received message: {message['data'].decode('utf-8')}")
+
+if __name__ == '__main__':
+    channel = 'my_channel'
+    listen_messages(channel)
--- web_face_detection/publisher.py
+++ web_face_detection/publisher.py
@@ -1,13 +1,35 @@
+# import redis
+# import time
+
+# r = redis.Redis(host='localhost', port=6379, db=0)
+# initiated=False
+
+# while True:
+#     if not initiated:
+#         print("publisher initiated")
+#         initiated=True
+#     message = f"Hello world! {time.time()}"
+#     r.publish('1', message)
+#     time.sleep(0.2)
+
+
+
 import redis
 import time
 
-r = redis.Redis(host='localhost', port=6379, db=0)
-initiated=False
+def publish_message(channel):
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    initiated = False
 
-while True:
-    if not initiated:
-        print("publisher initiated")
-        initiated=True
-    message = f"Hello world! {time.time()}"
-    r.publish('my_channel', message)
-    time.sleep(0.2)
+    while True:
+        if not initiated:
+            print("Publisher initiated")
+            initiated = True
+
+        message = f"Hello world! {time.time()}"
+        r.publish(channel, message)
+        time.sleep(0.2)
+
+if __name__ == '__main__':
+    channel = 'my_channel'
+    publish_message(channel)
